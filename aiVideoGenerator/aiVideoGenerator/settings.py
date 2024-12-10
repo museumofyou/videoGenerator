f1264183648431a7a10f02b4d0ed94489e12695a
denys yudin <denysyudin182@gmail.com>
2024-12-10 16:10:41 +0200

    fix settings

--- aiVideoGenerator/aiVideoGenerator/settings.py
+++ aiVideoGenerator/aiVideoGenerator/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
@@ -126,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
