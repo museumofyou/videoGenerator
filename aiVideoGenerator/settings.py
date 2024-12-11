04651151b269c45a7fe390c65f5dcb48ea4498ee
denys yudin <denysyudin182@gmail.com>
2024-12-11 07:53:51 +0200

    remove debug from settings

--- aiVideoGenerator/settings.py
+++ aiVideoGenerator/settings.py
@@ -16,7 +16,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG = True
 ALLOWED_HOSTS = ['*']
 PORT = os.environ.get('PORT', 8000)
 
